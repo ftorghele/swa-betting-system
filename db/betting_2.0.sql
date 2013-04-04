a97c0e4c7f4ac7a59a582f2f3bfa44b158e7e07f
Thomas Esterer <esterer.thomas@gmail.com>
2013-04-04 10:13:45 +0200

    updated database structure. added necessary fields for saving the quote

--- db/betting_2.0.sql
+++ db/betting_2.0.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Erstellungszeit: 04. Apr 2013 um 09:48
+-- Erstellungszeit: 04. Apr 2013 um 10:13
 -- Server Version: 5.5.25
 -- PHP-Version: 5.4.4
 
@@ -26,6 +26,11 @@ CREATE TABLE `bets` (
   `id_game` int(11) NOT NULL,
   `amount` double NOT NULL,
   `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `quote` double NOT NULL,
+  `quoteA` tinyint(1) NOT NULL DEFAULT '0',
+  `quoteB` tinyint(1) NOT NULL DEFAULT '0',
+  `quoteX` tinyint(1) NOT NULL DEFAULT '0',
+  `analyzed` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
@@ -33,8 +38,8 @@ CREATE TABLE `bets` (
 -- Daten für Tabelle `bets`
 --
 
-INSERT INTO `bets` (`id`, `id_user`, `id_game`, `amount`, `time`) VALUES
-(1, 2, 16, 10, '2013-04-04 07:46:46');
+INSERT INTO `bets` (`id`, `id_user`, `id_game`, `amount`, `time`, `quote`, `quoteA`, `quoteB`, `quoteX`, `analyzed`) VALUES
+(1, 2, 16, 10, '2013-04-04 07:46:46', 1.6, 0, 0, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -52,14 +57,16 @@ CREATE TABLE `games` (
   `endTime` datetime DEFAULT NULL,
   `transmitted` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=19 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=21 ;
 
 --
 -- Daten für Tabelle `games`
 --
 
 INSERT INTO `games` (`id`, `title`, `description`, `quoteA`, `quoteB`, `quoteX`, `endTime`, `transmitted`) VALUES
-(16, 'Benfica Lissabon - Newcastle United', 'asdf', 1.5, 3, 4.33, '2013-04-04 21:05:00', 0);
+(16, 'Benfica Lissabon - Newcastle United', 'asdf', 1.5, 3, 4.33, '2013-04-04 21:05:00', 0),
+(19, '1 gegen 2', 'test', 1.5, 6.2, 1.2, '2013-04-04 10:00:00', 1),
+(20, 'Fallen Christoph die Haare aus?', 'asdf', 1.5, 6.2, 1.2, '2013-04-04 10:04:40', 1);
 
 -- --------------------------------------------------------
 
